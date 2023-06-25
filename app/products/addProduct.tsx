0971daa4d09d0b47038b48a5823cfd27740af248
Cengiz YILMAZ <terabilgi@gmail.com>
2023-06-26 00:49:01 +0300

    Form alan adlarına göre e.target.value alınıyor

--- app/products/addProduct.tsx
+++ app/products/addProduct.tsx
@@ -1,14 +1,21 @@
-"use client"
+'use client'
 
-import { useState } from "react"
-import type {Brand} from "@prisma/client"
+import { SyntheticEvent, useState } from 'react'
+import type { Brand } from '@prisma/client'
 
-
-const AddProduct = ({brands}:{brands:Brand[]}) => {
+const AddProduct = ({ brands }: { brands: Brand[] }) => {
   const [isOpen, setisOpen] = useState(false)
 
   const handleModal = () => {
     setisOpen(!isOpen)
+  }
+
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState('')
+  const [brand, setBrand] = useState('')
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault
   }
 
   return (
@@ -19,32 +26,47 @@ const AddProduct = ({brands}:{brands:Brand[]}) => {
       <div className={isOpen ? 'modal modal-open' : 'modal'}>
         <div className='modal-box'>
           <h3 className='text-lg font-bold'>Add New Product</h3>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='w-full form-control'>
               <label className='font-bold label'>Product Name</label>
-              <input className='input input-bordered' placeholder='Product Name' />
+              <input
+                className='input input-bordered'
+                placeholder='Product Name'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </div>
             <div className='w-full form-control'>
               <label className='font-bold label'>Price</label>
-              <input className='input input-bordered' placeholder='Price' />
+              <input
+                className='input input-bordered'
+                placeholder='Price'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
             </div>
             <div className='w-full form-control'>
               <label className='font-bold label'>Brand</label>
-              <select className='w-full select select-bordered'>
-                <option disabled>
-                  Marka Seçiniz
-                </option>
+              <select
+                className='w-full select select-bordered'
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              >
+                <option disabled>Marka Seçiniz</option>
                 {brands.map((brand) => {
-                  return <option key={brand.id} value={brand.id}>{brand.name}</option>
-                }
-                )}
+                  return (
+                    <option key={brand.id} value={brand.id}>
+                      {brand.name}
+                    </option>
+                  )
+                })}
               </select>
             </div>
             <div className='modal-action'>
               <button type='button' className='btn' onClick={() => handleModal()}>
                 Close
               </button>
-              <button type='submit' className='btn btn-primary'>
+              <button type='submit' className='btn btn-primary' >
                 Submit
               </button>
             </div>
