158f3ffa559e6823459c6445870a8523019a7e66
Cengiz YILMAZ <terabilgi@gmail.com>
2023-06-26 19:17:44 +0300

    api product için POST yapısı

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,8 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import type { Product } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
 export const POST = async(request: Request) =>{
+    const body:Product = await request.json()
+    
+    // Prisma ile Db'ye kayıt
+    const product = await prisma.product.create({
+        data:{
+            title:body.title,
+            price:body.price,
+            brandId:body.brandId,
+        }
+    })
 
+    return NextResponse.json(product)
 }
